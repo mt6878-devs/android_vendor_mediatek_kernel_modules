f4c99efd1cb0d3ca9858ef8676a945ca77279be8
bengris32 <bengris32@protonmail.ch>
2024-03-07 21:01:18 +0000

    connectivity: gen4m: Use PM notifier to control WLAN suspend
    Change-Id: Iaa8df18c147b9dc6c940e90de6d98ee2f1cb7f51
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- connectivity/wlan/core/gen4m/os/linux/gl_init.c
+++ connectivity/wlan/core/gen4m/os/linux/gl_init.c
@@ -8343,7 +8343,7 @@ static int initWlan(void)
 #if (CFG_CHIP_RESET_SUPPORT)
 	glResetInit(prGlueInfo);
 #endif
-	kalFbNotifierReg(prGlueInfo);
+	kalNotifierReg(prGlueInfo);
 
 #if CFG_MODIFY_TX_POWER_BY_BAT_VOLT
 	kalBatNotifierReg(prGlueInfo);
@@ -8449,7 +8449,7 @@ static void exitWlan(void)
 	mddpUninit();
 #endif
 
-	kalFbNotifierUnReg();
+	kalNotifierUnReg();
 
 #if CFG_MODIFY_TX_POWER_BY_BAT_VOLT
 	kalBatNotifierUnReg();
@@ -8605,7 +8605,7 @@ static int wf_pdwnc_notify(struct notifier_block *nb,
 		mddpUninit();
 #endif
 		wlanUnregisterNetdevNotifier();
-		kalFbNotifierUnReg();
+		kalNotifierUnReg();
 
 #if CFG_MODIFY_TX_POWER_BY_BAT_VOLT
 		kalBatNotifierUnReg();
